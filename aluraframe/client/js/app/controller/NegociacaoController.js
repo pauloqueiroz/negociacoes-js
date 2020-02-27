fb772a94f88e2b7f118344efa160975db149f4ad
Paulo Sérgio <paulosergio@infoway-pi.com.br>
2020-02-27 13:45:12 -0300

    Testando método bind para criação de alias

--- aluraframe/client/js/app/controller/NegociacaoController.js
+++ aluraframe/client/js/app/controller/NegociacaoController.js
@@ -2,6 +2,14 @@ class NegociacaoController{
 
 	adiciona(event){
 		event.preventDefault();
-		alert("Chamou controller")
+
+		let $ = document.querySelector.bind(document);
+		let inputData = $("#data");
+		let inputQuantidade = $("#quantidade");
+		let inputValor = $("#valor");
+		console.log(inputData.value);
+		console.log(inputQuantidade.value);
+		console.log(inputValor.value);
+		alert("Chamou controller");
 	}
 }
