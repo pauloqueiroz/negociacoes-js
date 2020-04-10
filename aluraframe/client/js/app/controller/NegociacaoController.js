8c1ddb9df3c01f21e461825638c6074fcdb9566e
Paulo Sérgio <paulosergio@infoway-pi.com.br>
2020-04-10 18:49:29 -0300

    Criação de helper para separação de lógica de manipulação de datas.

--- aluraframe/client/js/app/controller/NegociacaoController.js
+++ aluraframe/client/js/app/controller/NegociacaoController.js
@@ -11,33 +11,15 @@ class NegociacaoController{
 	adiciona(event){
 		event.preventDefault();
 
-		// let dataConvertida = new Date(this.inputData.value.replace(/-/g, ','));
-		// let negociacao = new Negociacao(dataConvertida, this.inputQuantidade.value, this.inputValor.value);
-
-		/* o uso dos (...) chama-se spread operator, uma forma de representar que o array passado por parâmetro 
-		 * será desmembrado em parâmetros distintos da função, na sequência em que estão no array. 
-		 */
-
-		 /* arrow functions possui uma sintaxe mais curta quando comparada a uma expressão de função 
-		  * (function expression) e não tem seu próprio this, arguments, super ou new.target. Estas 
-		  * expressões de funções são melhor aplicadas para funções que não sejam métodos, e elas não 
-		  * podem ser usadas como construtoras (constructors).
-		  */
-		let dataConvertida = new Date(
-				...this.inputData.value.split("-").map(
-					(item, indice) => {
-						if(indice == 1){
-							return item-1;
-						}
-						return item;
-					}
-				)
-			);
-		console.log(dataConvertida);
-		var negociacao = new Negociacao(dataConvertida, this.inputQuantidade.value, this.inputValor.value);
-		console.log(this.inputQuantidade.value);
-		console.log(this.inputValor.value);
+		let helper = new DateHelper(); 
+		
+		var negociacao = new Negociacao(
+								helper.textoParaData(this.inputData.value), 
+								this.inputQuantidade.value, 
+								this.inputValor.value
+								);
 		console.log(negociacao);
+		console.log(helper.dataParaTexto(negociacao.data));
 		alert("Chamou controller");
 	}
 }
---
+++ aluraframe/client/js/app/helper/DateHelper.js
@@ -0,0 +1,37 @@
+class DateHelper{
+	
+
+	// let dataConvertida = new Date(this.inputData.value.replace(/-/g, ','));
+	// let negociacao = new Negociacao(dataConvertida, this.inputQuantidade.value, this.inputValor.value);
+
+	/* o uso dos (...) chama-se spread operator, uma forma de representar que o array passado por parâmetro 
+	 * será desmembrado em parâmetros distintos da função, na sequência em que estão no array. 
+	 */
+
+	 /* arrow functions possui uma sintaxe mais curta quando comparada a uma expressão de função 
+	  * (function expression) e não tem seu próprio this, arguments, super ou new.target. Estas 
+	  * expressões de funções são melhor aplicadas para funções que não sejam métodos, e elas não 
+	  * podem ser usadas como construtoras (constructors).
+	  */
+
+	textoParaData(texto){
+		return new Date(
+				...texto
+					.split("-")
+					.map(
+						(item, indice) => {
+							if(indice == 1){
+								return item-1;
+							}
+							return item;
+						}
+				)
+			);
+	}
+
+	dataParaTexto(data){
+		return data.getDate() 
+				+ "/" + (data.getMonth()+1)
+				+ "/" + (data.getFullYear());
+	}
+}
