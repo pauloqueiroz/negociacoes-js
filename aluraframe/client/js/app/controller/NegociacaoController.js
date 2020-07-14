670c2b0dc1745ddc6817765cb7925531ec6217db
Paulo Sérgio <paulosergio@infoway-pi.com.br>
2020-07-13 23:26:18 -0300

    Criação de view através de template para inclusão de novos dados na tabela de negociações

--- aluraframe/client/js/app/controller/NegociacaoController.js
+++ aluraframe/client/js/app/controller/NegociacaoController.js
@@ -6,6 +6,8 @@ class NegociacaoController{
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
 		this._listaNegociacoes = new ListaNegociacoes();
+		this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+		this._negociacoesView.update(this._listaNegociacoes);
 	}
 
 	adiciona(event){
@@ -13,7 +15,7 @@ class NegociacaoController{
 		
 		var negociacao = this._criaNegociacao();
 		this._listaNegociacoes.adiciona(negociacao);
-
+		this._negociacoesView.update(this._listaNegociacoes);
 		this._limpaFormulario();
 		console.log(this._listaNegociacoes);
 	}
@@ -29,7 +31,7 @@ class NegociacaoController{
 	_limpaFormulario(){
 		this._inputData.value = '';
 		this._inputQuantidade.value = 1;
-		this._inputValor = 0.0;
+		this._inputValor.value = 0.0;
 		this._inputData.focus();
 	}
 }
